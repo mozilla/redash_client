074bae055f2ad6dcbff09c783eeaf931ccdf65ac
Marina Samuel <msamuel@mozilla.com>
2017-03-27 17:15:05 -0400

    Add table schema for JSON URL Fetcher data source.

--- constants.py
+++ constants.py
@@ -16,3 +16,31 @@ class ChartType:
   LINE = "line"
   SCATTER = "scatter"
   AREA = "area"
+
+TtableSchema = [
+  {
+    "name": "Metric",
+    "type": "string",
+    "friendly_name": "Metric"
+  },
+  {
+    "name": "Alpha Error",
+    "type": "float",
+    "friendly_name": "Alpha Error"
+  },
+  {
+    "name": "Power",
+    "type": "float",
+    "friendly_name": "Power"
+  },
+  {
+    "name": "Two-Tailed P-value (ttest)",
+    "type": "float",
+    "friendly_name": "Two-Tailed P-value (ttest)"
+  },
+  {
+    "name": "Experiment Mean - Control Mean",
+    "type": "float",
+    "friendly_name": "Experiment Mean - Control Mean"
+  }
+]
