b00a677eb545e822e40462f65806712cefe48fde
Marina Samuel <msamuel@mozilla.com>
2017-08-15 17:26:58 -0400

    Add setup files for publishing.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+  name = 'redash_client',
+  packages = ['redash_client'],
+  version = '0.1.1',
+  description = 'A client for the re:dash API for stmo (https://sql.telemetry.mozilla.org)',
+  author = 'Marina Samuel',
+  author_email = 'msamuel@mozilla.com',
+  url = 'https://github.com/mozilla/redash_client',
+  keywords = ['redash', 'experiments', 'a/b tests'],
+  classifiers = [],
+)
