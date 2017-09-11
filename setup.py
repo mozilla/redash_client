2cb05b1db1af0b4afdc5d4ac019c94e74bffa466
Marina Samuel <msamuel@mozilla.com>
2017-09-11 16:46:24 -0400

    Pip packaging should include dashboards subdirectory.

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from distutils.core import setup
 setup(
   name = 'redash_client',
-  packages = ['redash_client'],
-  version = '0.1.4',
+  packages = ['redash_client', 'redash_client.dashboards'],
+  version = '0.1.5',
   description = 'A client for the re:dash API for stmo (https://sql.telemetry.mozilla.org)',
   author = 'Marina Samuel',
   author_email = 'msamuel@mozilla.com',
