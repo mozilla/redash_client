f44dbd3793a1fac53db0fb28ec0b850369557a60
Marina Samuel <msamuel@mozilla.com>
2018-01-31 14:16:04 -0500

    Closes #63: Add statistical libraries to setup.py.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 setup(
   name = 'redash_client',
   packages = ['redash_client', 'redash_client.dashboards'],
-  version = '0.1.9',
+  version = '0.1.13',
   description = 'A client for the re:dash API for stmo (https://sql.telemetry.mozilla.org)',
   author = 'Marina Samuel',
   author_email = 'msamuel@mozilla.com',
@@ -13,5 +13,9 @@ setup(
     "boto3 == 1.4.4",
     "requests == 2.12.1",
     "python-slugify == 1.2.4",
+    "scipy == 1.0.0",
+    "statistics == 1.0.3.5",
+    "statsmodels == 0.6.1",
+    "urllib3 == 1.21.1"
   ]
 )
