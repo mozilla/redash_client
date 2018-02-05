bc402be8ce794b3fc24d00e4018fa6a9e3a3f859
Marina Samuel <msamuel@mozilla.com>
2018-02-05 14:45:58 -0500

    Closes #61: Update constant.py and tests/base.py accordingly.

--- redash_client/constants.py
+++ redash_client/constants.py
@@ -1,8 +1,3 @@
-class RetentionType:
-  DAILY = "day"
-  WEEKLY = "week"
-
-
 class TimeInterval:
   DAILY = "daily"
   WEEKLY = "weekly"
@@ -27,39 +22,3 @@ class ChartType:
   SCATTER = "scatter"
   AREA = "area"
   allowed_chart_types = [BAR, PIE, LINE, SCATTER, AREA]
-
-
-TTableSchema = [
-    {
-        "name": "Metric",
-        "type": "string",
-        "friendly_name": "Metric"
-    }, {
-        "name": "Alpha Error",
-        "type": "float",
-        "friendly_name": "Alpha Error"
-    }, {
-        "name": "Power",
-        "type": "float",
-        "friendly_name": "Power"
-    }, {
-        "name": "Two-Tailed P-value (ttest)",
-        "type": "float",
-        "friendly_name": "Two-Tailed P-value (ttest)"
-    }, {
-        "name": "Control Mean",
-        "type": "float",
-        "friendly_name": "Control Mean"
-    }, {
-        "name": "Experiment Mean - Control Mean",
-        "type": "float",
-        "friendly_name": "Experiment Mean - Control Mean"
-    }, {
-        "name": "Percent Difference in Means",
-        "type": "float",
-        "friendly_name": "Percent Difference in Means"
-    }, {
-        "name": "Significance",
-        "type": "string",
-        "friendly_name": "Significance"
-    }]
--- redash_client/tests/base.py
+++ redash_client/tests/base.py
@@ -2,71 +2,8 @@ import mock
 import json
 import unittest
 
-from redash_client.client import RedashClient
-from redash_client.dashboards.SummaryDashboard import SummaryDashboard
-
 
 class AppTest(unittest.TestCase):
-
-  def post_server(self, url, data):
-    EXPECTED_QUERY_ID = "query_id123"
-    EXPECTED_QUERY_STRING = "select some_stuff from table"
-    QUERY_ID_RESPONSE = {
-        "id": EXPECTED_QUERY_ID
-    }
-    FORK_RESPONSE = {
-        "id": EXPECTED_QUERY_ID,
-        "query": EXPECTED_QUERY_STRING,
-        "data_source_id": 3
-    }
-
-    response = self.get_mock_response(
-        content=json.dumps(QUERY_ID_RESPONSE))
-
-    if "fork" in url:
-      response = self.get_mock_response(
-          content=json.dumps(FORK_RESPONSE))
-
-    self.server_calls += 1
-    return response
-
-  def get_dashboard(self, api_key):
-    EVENTS_TABLE_NAME = "activity_stream_mobile_events_daily"
-    START_DATE = "02/17/2017"
-    DASH_NAME = "Firefox iOS: Metrics Summary"
-
-    self.mock_requests_get.return_value = self.get_mock_response()
-    self.mock_requests_post.return_value = self.get_mock_response()
-
-    dashboard = SummaryDashboard(
-        self.redash,
-        DASH_NAME,
-        EVENTS_TABLE_NAME,
-        START_DATE,
-    )
-    return dashboard
-
-  def setUp(self):
-    API_KEY = "test_key"
-
-    self.redash = RedashClient(API_KEY)
-
-    mock_requests_post_patcher = mock.patch(
-        "redash_client.client.requests.post")
-    self.mock_requests_post = mock_requests_post_patcher.start()
-    self.addCleanup(mock_requests_post_patcher.stop)
-
-    mock_requests_get_patcher = mock.patch(
-        "redash_client.client.requests.get")
-    self.mock_requests_get = mock_requests_get_patcher.start()
-    self.addCleanup(mock_requests_get_patcher.stop)
-
-    mock_requests_delete_patcher = mock.patch(
-        "redash_client.client.requests.delete")
-    self.mock_requests_delete = mock_requests_delete_patcher.start()
-    self.addCleanup(mock_requests_delete_patcher.stop)
-
-    self.dash = self.get_dashboard(API_KEY)
 
   def get_mock_response(self, status=200, content='{}'):
     mock_response = mock.Mock()
