c6198681e3284506e60b0f9b2de4b8ad40619923
Marina Samuel <msamuel@mozilla.com>
2018-02-05 14:57:39 -0500

    Closes #61: Update setup.py to reflect pulled out dashboard code.

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 setup(
   name = 'redash_client',
-  packages = ['redash_client', 'redash_client.dashboards'],
-  version = '0.1.19',
+  packages = ['redash_client'],
+  version = '0.2.0',
   description = 'A client for the re:dash API for stmo (https://sql.telemetry.mozilla.org)',
   author = 'Marina Samuel',
   author_email = 'msamuel@mozilla.com',
@@ -10,12 +10,8 @@ setup(
   keywords = ['redash', 'experiments', 'a/b tests'],
   classifiers = [],
   install_requires=[
-    "boto3 == 1.4.4",
     "requests == 2.12.1",
     "python-slugify == 1.2.4",
-    "scipy == 1.0.0",
-    "statistics == 1.0.3.5",
-    "statsmodels == 0.6.1",
     "urllib3 == 1.21.1"
   ]
 )
