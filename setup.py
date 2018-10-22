6e283ae557df781297d81a3edba72997038cc517
Peter Bengtsson <mail@peterbe.com>
2018-10-22 15:41:12 -0400

    README missing on PyPI
    Fixes #86

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
+import os
 from setuptools import setup
+
+_here = os.path.dirname(__file__)
+
 setup(
   name = 'redash_client',
   packages = ['redash_client'],
   version = '0.2.4',
   description = 'A client for the Redash API for stmo (https://sql.telemetry.mozilla.org)',
+  long_description= open(os.path.join(_here, 'README.rst')).read(),
   author = 'Marina Samuel',
   author_email = 'msamuel@mozilla.com',
   url = 'https://github.com/mozilla/redash_client',
